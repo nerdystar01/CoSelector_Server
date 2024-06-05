05e8329184e60eed5beb49aee24863947e25dd23
nerdystar <nerdystar@nerdystarui-MacBookPro.local>
2024-06-05 17:11:48 +0900

    [6/3][MOD]: 파일 수정

--- api/views.py
+++ api/views.py
@@ -4,22 +4,11 @@ from rest_framework import status
 from rest_framework import serializers
 from coselector.coselector_0527 import process_pick_with_api  # Ensure this is the correct import path
 
-class CoSelectorItemSerializer(serializers.Serializer):
-    resource_id = serializers.IntegerField()
-    image_np = serializers.CharField(max_length=100)
-    similarity_score = serializers.FloatField(required=False, allow_null=True)
-
-class CoSelectorSerializer(serializers.ListSerializer):
-    child = CoSelectorItemSerializer()
-
 
 class CoSelectorView(APIView):
-    def post(self, request, *args, **kwargs):
-        serializer = CoSelectorSerializer(data=request.data)
-        if serializer.is_valid():
-            data = serializer.validated_data['data']
-            response_data = process_pick_with_api(data)
-            return Response(response_data, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def post(self, request):
+        request_list = request.data
+        print(request_list)
+        response_data = process_pick_with_api(request_list)
+        return Response(response_data, status=status.HTTP_201_CREATED)
     
